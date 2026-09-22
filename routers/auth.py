12b1b3d800654856e1db4adec31257bca206fbf4
Maryamh12 <maryam_hazrati94@yahoo.com>
2026-09-22 22:53:13 +0100

    Revision router folder

--- routers/auth.py
+++ routers/auth.py
@@ -1,92 +1,109 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
+
 from app import models, schemas
 from app.database import get_db
-from app.security import get_current_user
+from app.security import (
+    hash_password,
+    verify_password,
+    create_access_token,
+    get_current_user
+)
 
 
 router = APIRouter(
-    prefix="/taks",
-    tags=["Tasks"]
+    prefix="/auth",
+    tags=["Authentication"]
 )
-
-def get_user_task(
-        task_id: int,
-        current_user: models.User,
-        db: Session
-) -> models.Task:
-
-    task = db.query(models.Task).filter(
-        models.Task.id == task_id
-    ).first()
-
-    if task is None:
-        raise HTTPException(
-            status_code=404,
-            detail="Task not found"
-        )
-
-    if task.owner_id != current_user.id:
-        raise HTTPException(
-            status_code=403,
-            detail="You do not have permission to access this task"
-        )
-
-    return task
-
-
-@router.get(
-    "",
-    response_model=list[schemas.TaskResponse]
-)
-
-def get_tasks(
-        current_user: models.User = Depends(get_current_user)
-):
-
-    return current_user.tasks
 
 
 @router.post(
-    "",
-    response_model=schemas.TaskResponse,
+    "/register",
+    response_model=schemas.UserResponse,
     status_code=201
 )
-
-def create_task(
-    task: schemas.TaskCreate,
-    db: Session = Depends(get_db),
-        current_user: models.User = Depends(get_current_user)
+def register_user(
+        user: schemas.UserCreate,
+        db: Session = Depends(get_db)
 ):
+    existing_email = db.query(models.User).filter(
+        models.User.email == user.email
+    ).first()
 
-    new_task = models.Task(
-        title=task.title,
-        description=task.description,
-        completed=task.completed,
-        owner=current_user
+    if existing_email:
+        print(existing_email)
+        raise HTTPException(
+            status_code=400,
+            detail="Email already registered"
+        )
+    existing_username = db.query(models.User).filter(
+        models.User.username == user.username
+    ).first()
+
+    if existing_username:
+        print(existing_username)
+        raise HTTPException(
+            status_code=400,
+            detail="Username already registered"
+        )
+
+    new_user = models.User(
+        username=user.username,
+        email=user.email,
+        hashed_password=hash_password(user.password)
     )
 
-    db.add(new_task)
+    db.add(new_user)
     db.commit()
-    db.refresh()
+    db.refresh(new_user)
 
-    return new_task
+    return new_user
+
+
+@router.post(
+    "/login",
+    response_model=schemas.Token
+)
+def login_user(
+        login_data: schemas.UserLogin,
+        db: Session = Depends(get_db)
+):
+    user = db.query(models.User).filter(
+        models.User.email == login_data.email
+    ).first()
+
+    if user is None:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    if not verify_password(
+            login_data.password,
+            user.hashed_password
+    ):
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+    access_token = create_access_token(
+        {
+            "sub": str(user.id)
+        }
+    )
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
 
 
 @router.get(
-    "/{task_id",
-    response_model=schemas.TaskResponse
+    "/me",
+    response_model=schemas.UserResponse
 )
-
-def get_task(
-        task_id: int,
-        db: Session = Depends(get_db),
+def get_me(
         current_user: models.User = Depends(get_current_user)
 ):
-    return  get_user_task(
-        task_id,
-        current_user,
-        db
-    )
 
+    return current_user
